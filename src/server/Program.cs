8762c6ecea47ad09e617786c3c19da261a6ecbcb
Marcel C <14852157+Marcel0024@users.noreply.github.com>
2023-02-28 10:00:46 +0100

    Remove unnecessary Authorization and Routing in the pipeline

--- src/server/Program.cs
+++ src/server/Program.cs
@@ -29,19 +29,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
 app.MapRazorPages();
+
 app.MapHub<GameHub>("/gameHub", options =>
 {
     options.TransportMaxBufferSize = 1024;
